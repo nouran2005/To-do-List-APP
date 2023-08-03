e40249fef14bb948a75ea4395096eb1aaeba77b3
Nouran <nouransamer5@gmail.com>
2023-08-03 15:08:52 +0300

    Home screen

---
+++ lib/Views/iist_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class listView extends StatelessWidget {
+  const listView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
